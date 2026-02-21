9453368deb84a798f9c703de8a6edcdd540b55cc
Anglesvar <anglesvar.cheenu98@gmail.com>
2026-02-21 11:53:57 +0530

    add app component

---
+++ src/app/app.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './features/home/home.component';
+import { AboutComponent } from './features/about/about.component';
+import { ProjectsComponent } from './features/projects/projects.component';
+import { ContactComponent } from './features/contact/contact.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    ProjectsComponent,
+    ContactComponent,
+  ],
+  template: `
+    <app-navbar />
+    <main>
+      <app-home />
+      <app-about />
+      <app-projects />
+      <app-contact />
+    </main>
+    <app-footer />
+  `,
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {}
